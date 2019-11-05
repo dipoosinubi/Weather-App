4111037f2f1843c3303343b4213a9ba6d51ca224
oladipo osinubi <drkrdnblk@oladipos-MacBook-Pro.local>
2019-11-04 21:15:01 -0500

    added API key and api call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import Titles from './components/Titles'
-import Form from './components/Form'
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
 import './App.css';
 
+
+const API_KEY = "87a0c01d35b2a8bf04a72265c54d3a06";
+
 class App extends React.Component {
-
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Atlanta,usa&appid=${API_KEY}&units=imperial`);
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
-
     return (
       <div className="App">
-        <Titles/>
-        <Form />
+        <Titles />
+        <Form  getWeather={this.getWeather}/>
+        <Weather />
       </div>
     );
   }
