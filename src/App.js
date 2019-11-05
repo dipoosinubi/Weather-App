8f062d7a3f216f557a27587d2f327c4761f8fd37
oladipo osinubi <drkrdnblk@oladipos-MacBook-Pro.local>
2019-11-04 21:40:22 -0500

    added state and set state

--- src/App.js
+++ src/App.js
@@ -8,16 +8,37 @@ import './App.css';
 const API_KEY = "87a0c01d35b2a8bf04a72265c54d3a06";
 
 class App extends React.Component {
-  getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Atlanta,usa&appid=${API_KEY}&units=imperial`);
+
+    state = {
+      temperature: undefined,
+      city: undefined,
+      country: undefined,
+      humidity: undefined,
+      description: undefined,
+      error: undefined,
+    }
+
+  getWeather = async (event) => {
+    event.preventDefault();
+    const city = event.target.elements.city.value;
+    const country = event.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
     const data = await api_call.json();
     console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error:""
+    })
   }
   render() {
     return (
       <div className="App">
         <Titles />
-        <Form  getWeather={this.getWeather}/>
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
