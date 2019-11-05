b794985fbb68a76334d16d5af330025dc698606f
oladipo osinubi <drkrdnblk@oladipos-MacBook-Pro.local>
2019-11-04 20:29:21 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+            welcome
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
