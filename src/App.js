24547eb837371fd695b5683167025b4080715407
oladipo osinubi <drkrdnblk@oladipos-MacBook-Pro.local>
2019-11-04 20:37:56 -0500

    created a form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Titles from './components/Titles'
+import Form from './components/Form'
 import './App.css';
 
 class App extends React.Component {
@@ -9,6 +10,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Titles/>
+        <Form />
       </div>
     );
   }
